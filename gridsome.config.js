199196bc16434f37cc0bf00f4fefab8314df0bed
Eric Fortney <efortney28@gmail.com>
2020-08-01 13:24:45 -0500

    base commit

---
+++ gridsome.config.js
@@ -0,0 +1,27 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'CAM Construction',
+  sitURL: 'https://camcustom.com',
+  siteDescription: 'Custom residential construction work.',
+  icon: 'src/favicon.png',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/work/**/*.md',
+        typeName: 'WorkEntry'
+      }
+    },
+    {
+      use: 'gridsome-plugin-netlify-cms',
+      options: {
+        publicPath: '/admin'
+      }
+    },
+  ]
+}
---
+++ gridsome.server.js
@@ -0,0 +1,7 @@
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = function (api) {
+  api.loadSource(store => {
+    //data store api
+  })
+}
