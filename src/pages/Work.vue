92bba027db2e8259e8465658570fe9d217a55f71
Eric Fortney <efortney28@gmail.com>
2020-10-18 12:11:35 -0500

    link opens in new tab

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -3,7 +3,7 @@
         <div class="container-fluid" id="image-container">
             <div class="row">
                 <div class="col-sm work-image" v-for="work in $page.work.edges">
-                    <a :href="work.node.images">
+                    <a :href="work.node.images" target="_blank">
                         <b-img thumbnail fluid width="200" height="200" :src="work.node.images"></b-img>
                     </a>
                 </div>
